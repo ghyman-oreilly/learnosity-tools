944dfac6645e8a090138f8c4d0d5e4a65c2e0e0b
Gregory Hyman <ghyman@remmac06548.local>
2025-03-13 10:52:20 -0400

    add methods for serializing quiz objects, for easier testing

--- src/classModules/tags.js
+++ src/classModules/tags.js
@@ -1,9 +1,16 @@
+const { deepSerialize } = require('../shared/helpers')
+
 
 class Tag {
 	constructor(TagName, TagValue) {
 		this.name = TagName;
 		this.value = [TagValue];
 	}
+
+	toJSON() {
+		/* object serialization for testing */
+    	return deepSerialize(this);
+  	}
 
 	appendValue(TagValue) {
 		this.value.push(TagValue);
